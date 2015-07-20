3b4b6fe9c4f51185f42bc3104cd49914096531bc
David Shao <dshao@dshao-mac01.local>
2015-07-20 00:42:51 -0700

    change gitignore

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
         # Configuration for Amazon S3
         :provider              => 'AWS',
-        :aws_access_key_id     => ENV['AWSAccessKeyId=AKIAJ5KXYDE2UOKGQUNA'],
-        :aws_secret_access_key => ENV['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']
+        :aws_access_key_id     => 'AWSAccessKeyId=AKIAJ5KXYDE2UOKGQUNA',
+        :aws_secret_access_key => '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'
     }
-    config.fog_directory     =  ENV['internhomeuw']
+    config.fog_directory     =  'internhomeuw'
   end
 end
