bca45493889d25d7536a6996b4e718661e4a0e4c
David Shao <dshao@dshao-mac01.local>
2015-08-03 02:12:48 -0700

    add preview link

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,4 +1,31 @@
 module MicropostsHelper
+  def process_content( url)
 
+      info = LinkThumbnailer.generate(url)
 
+      content_tag :span, :class=> "content" do
+         content_tag(:h3, :class=> "content") do
+            link_to(info.title, url)
+         end
+      end
+
+      content_tag(:span,  info.description, class: "content")
+      #
+     # content_tag(:span,  favicon_link_tag(info.favicon), class: "content")
+
+      #if LinkThumbnailer.generate(url).images.size > 0
+          #  content_tag(:span,  image_tag(info.images.first.src.to_s, size: "200"), class: "content")
+     # end
+
+  end
 end
+
+
+
+
+
+
+
+
+
+
