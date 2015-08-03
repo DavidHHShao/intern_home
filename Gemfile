e29b7eda18a1b8a01ed43997239f65133f1645a3
David Shao <dshao@dshao-mac01.local>
2015-08-02 18:58:22 -0700

    add preview link

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+# link preview
+gem 'link_thumbnailer'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,2 +1,13 @@
 module MicropostsHelper
+
+
+  # Returns the preview link for the given micropost.
+  def preview_link_for(micropost)
+    urls = URI.extract(micropost.content)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
 end
